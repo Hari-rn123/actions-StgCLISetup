139f57209a4911b96d1fe2ba4a4a7bed8816fb92
Hari-rn123 <harikrishnanu@linkites.com>
2024-02-22 11:29:20 +0530

    new path changes

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
+// const core = require("@actions/core");
+// const github = require("@actions/github");
+
+// try {
+//   // `who-to-greet` input defined in action metadata file
+//   const nameToGreet = core.getInput("who-to-greet");
+//   console.log(`Hello ${nameToGreet}!`);
+//   const time = new Date().toTimeString();
+//   core.setOutput("time", time);
+//   // Get the JSON webhook payload for the event that triggered the workflow
+//   const payload = JSON.stringify(github.context.payload, undefined, 2);
+//   console.log(`The event payload: ${payload}`);
+// } catch (error) {
+//   core.setFailed(error.message);
+// }
+
 const core = require("@actions/core");
-const github = require("@actions/github");
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  // const workingDirectory = core.getInput('working-directory');
+  const workingDirectory = process.cwd();
+  const homedir = os.homedir();
+  const relativePath = path.relative(homedir, workingDirectory);
+  core.debug(
+    `path metioned are workingDirectory: ${workingDirectory} homedir: ${homedir} relativePath: ${relativePath} `
+  );
+  core.debug(`Setting working directory to: ${workingDirectory}`);
+  process.chdir(workingDirectory);
 } catch (error) {
   core.setFailed(error.message);
 }
